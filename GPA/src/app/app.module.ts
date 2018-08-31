adc8affbf21d2f641289d1155e25a2d3e1dc6247
unknown <pbgaya1993@gmail.com>
2018-08-31 23:34:36 +0530

    create navbar

--- GPA/src/app/app.module.ts
+++ GPA/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { GpaviewComponent } from './component/gpaview/gpaview.component';
 import { ResultviewComponent } from './component/resultview/resultview.component';
+import { ResultEnterComponent } from './component/result-enter/result-enter.component';
+import { UserRegisterComponent } from './component/user-register/user-register.component';
 
 const appRoutes: Routes=[
 {path: 'gpa', component : GpaviewComponent},
 {path: 'result', component : ResultviewComponent },
 {path: 'home', component :  AppComponent  },
+{path: 'result_enter', component : ResultEnterComponent},
+{path: 'user_register', component : UserRegisterComponent}
 
 ];
 
@@ -18,7 +22,9 @@ const appRoutes: Routes=[
     AppComponent,
     NavbarComponent,
     GpaviewComponent,
-    ResultviewComponent
+    ResultviewComponent,
+    ResultEnterComponent,
+    UserRegisterComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
